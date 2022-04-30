e16e90ed752e6951b5f5a637116c58e02a211f13
Jordon <jordontgarcia@gmail.com>
2022-04-29 20:49:02 -0700

    WIP Search Feature Implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
 	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+	plugins: [require('@tailwindcss/forms')],
 };
