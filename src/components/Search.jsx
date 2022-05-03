637f70fe79dff9cc25f7f0b1f6b14d71ba3173cf
Jordon <jordontgarcia@gmail.com>
2022-05-03 00:25:00 -0700

    Commented out file

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,81 +1,81 @@
-import { useState } from 'react';
-import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
-import { Combobox } from '@headlessui/react';
+// import { useState } from 'react';
+// import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
+// import { Combobox } from '@headlessui/react';
 
-const project = [
-	{ id: 1, projectName: '' },
-	{ id: 2, projectName: '' },
-	{ id: 3, projectName: '' },
-];
+// const project = [
+// 	{ id: 1, projectName: '' },
+// 	{ id: 2, projectName: '' },
+// 	{ id: 3, projectName: '' },
+// ];
 
-function classNames(...classes) {
-	return classes.filter(Boolean).join(' ');
-}
+// function classNames(...classes) {
+// 	return classes.filter(Boolean).join(' ');
+// }
 
-const Search = () => {
-	const [query, setQuery] = useState('');
-	const [selectedProject, setSelectedProject] = useState();
+// const Search = () => {
+// 	const [query, setQuery] = useState('');
+// 	const [selectedProject, setSelectedProject] = useState();
 
-	const filteredProject =
-		query === ''
-			? project
-			: project.filter((project) => {
-					return project.projectName.toLowerCase().includes(query.toLowerCase());
-			  });
+// 	const filteredProject =
+// 		query === ''
+// 			? project
+// 			: project.filter((project) => {
+// 					return project.projectName.toLowerCase().includes(query.toLowerCase());
+// 			  });
 
-	return (
-		<Combobox
-			as='div'
-			value={selectedProject}
-			onChange={setSelectedProject}
-			className='md:container md:max-w-screen-xl md:mx-auto'
-		>
-			<Combobox.Label className='block text-sm font-medium text-gray-700'>Assigned to</Combobox.Label>
-			<div className='relative mt-1'>
-				<Combobox.Input
-					className='w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm'
-					onChange={(event) => setQuery(event.target.value)}
-				/>
-				<Combobox.Button className='absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none'>
-					<SelectorIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
-				</Combobox.Button>
+// 	return (
+// 		<Combobox
+// 			as='div'
+// 			value={selectedProject}
+// 			onChange={setSelectedProject}
+// 			className='md:container md:max-w-screen-xl md:mx-auto'
+// 		>
+// 			<Combobox.Label className='block text-sm font-medium text-gray-700'>Assigned to</Combobox.Label>
+// 			<div className='relative mt-1'>
+// 				<Combobox.Input
+// 					className='w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm'
+// 					onChange={(event) => setQuery(event.target.value)}
+// 				/>
+// 				<Combobox.Button className='absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none'>
+// 					<SelectorIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
+// 				</Combobox.Button>
 
-				{filteredProject.length > 0 && (
-					<Combobox.Options className='absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
-						{filteredProject.map((project) => (
-							<Combobox.Option
-								key={project.id}
-								value={project}
-								className={({ active }) =>
-									classNames(
-										'relative cursor-default select-none py-2 pl-3 pr-9',
-										active ? 'bg-indigo-600 text-white' : 'text-gray-900',
-									)
-								}
-							>
-								{({ active, selected }) => (
-									<>
-										<span className={classNames('block truncate', selected && 'font-semibold')}>{project.id}</span>
+// 				{filteredProject.length > 0 && (
+// 					<Combobox.Options className='absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
+// 						{filteredProject.map((project) => (
+// 							<Combobox.Option
+// 								key={project.id}
+// 								value={project}
+// 								className={({ active }) =>
+// 									classNames(
+// 										'relative cursor-default select-none py-2 pl-3 pr-9',
+// 										active ? 'bg-indigo-600 text-white' : 'text-gray-900',
+// 									)
+// 								}
+// 							>
+// 								{({ active, selected }) => (
+// 									<>
+// 										<span className={classNames('block truncate', selected && 'font-semibold')}>{project.id}</span>
 
-										{selected && (
-											<span
-												className={classNames(
-													'absolute inset-y-0 right-0 flex items-center pr-4',
-													active ? 'text-white' : 'text-indigo-600',
-												)}
-											>
-												<CheckIcon className='h-5 w-5' aria-hidden='true' />
-											</span>
-										)}
-									</>
-								)}
-							</Combobox.Option>
-						))}
-					</Combobox.Options>
-				)}
-			</div>
-		</Combobox>
-	);
-};
+// 										{selected && (
+// 											<span
+// 												className={classNames(
+// 													'absolute inset-y-0 right-0 flex items-center pr-4',
+// 													active ? 'text-white' : 'text-indigo-600',
+// 												)}
+// 											>
+// 												<CheckIcon className='h-5 w-5' aria-hidden='true' />
+// 											</span>
+// 										)}
+// 									</>
+// 								)}
+// 							</Combobox.Option>
+// 						))}
+// 					</Combobox.Options>
+// 				)}
+// 			</div>
+// 		</Combobox>
+// 	);
+// };
 
-export default Search;
+// export default Search;
