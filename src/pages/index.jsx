8d35183859c963d4664d4748ae7038096ea2045f
Jordon <jordontgarcia@gmail.com>
2022-05-04 19:29:54 -0700

    Changed BG Color to match CDC Branding

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,14 @@ import Search from '../components/Search';
 
 const Home = () => {
 	return (
-		<div className='h-full bg-slate-900'>
+		<div className='h-full bg-gray-100'>
 			<div className='max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8'>
 				<div className='text-center'>
-					<h2 className='text-lg font-semibold text-blue-200 tracking-wide uppercase'>FOSS @ CDC</h2>
-					<p className='mt-1 text-2xl font-bold text-gray-200 sm:text-3xl sm:tracking-tight lg:text-4xl'>
+					<h2 className='text-lg font-semibold text-blue-500 tracking-wide uppercase'>FOSS @ CDC</h2>
+					<p className='mt-1 text-2xl font-bold text-gray-800 sm:text-3xl sm:tracking-tight lg:text-4xl'>
 						Free, Open-Source Software Built By CDC
 					</p>
-					<p className='max-w-xl mt-5 mx-auto text-xl text-gray-300'>
+					<p className='max-w-xl mt-5 mx-auto text-xl text-gray-700'>
 						Tools for Managing, Visualizing, Analyzing Public Health Data
 					</p>
 				</div>
