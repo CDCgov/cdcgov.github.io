0fa3b973f518fe7719112c3e81208ba03f78f5db
Jordon <jordontgarcia@gmail.com>
2022-05-04 19:29:25 -0700

    Updated Search feature, components now hide

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,10 +9,10 @@ import { Combobox } from '@headlessui/react';
 
 console.clear();
 const Projects = () => {
-	let apiData = [
-		{ id: 1, projectName: '' },
-		{ id: 2, projectName: '' },
-	];
+	// let apiData = [
+	// 	{ id: 1, projectName: '' },
+	// 	{ id: 2, projectName: '' },
+	// ];
 
 	const [projects, setProjects] = useState([]);
 	const [errorMessage, setErrorMessage] = useState(null);
@@ -45,32 +45,44 @@ const Projects = () => {
 			);
 	}, [endpoints]);
 
-	let propId = Object.getOwnPropertyNames(apiData);
+	//let propId = Object.getOwnPropertyNames(apiData);
 
-	for (let i = 0; i < projects.length; i++) {
-		if (propId.length < projects.length) {
-			apiData.unshift({
-				id: i,
-				projectName: projects[i].name,
-				projectLang: projects[i].language,
-			});
-		}
-	}
+	// for (let i = 0; i < projects.length; i++) {
+	// 	if (propId.length < projects.length) {
+	// 		apiData.unshift({
+	// 			id: i,
+	// 			projectFullName: projects[i].full_name,
+	// 			projectName: projects[i].name,
+	// 			projectLang: projects[i].language,
+	// 		});
+	// 	}
+	// }
 
-	// TODO: Separate the search component into another file, for easier maintenance.
+	const searchText = (event) => {
+		setQuery(event.target.value);
+	};
+
+	const filterApiData = !query
+		? projects
+		: projects.filter((projects) => {
+				return projects.name.toLowerCase().includes(query.toLowerCase());
+		  });
+
+	let projectSearch = projects.filter((item) => {
+		return item.name.toLowerCase().includes(query.toLowerCase());
+	});
+
 	function classNames(...classes) {
 		return classes.filter(Boolean).join(' ');
 	}
 
-	const filterApiData =
-		query === ''
-			? apiData
-			: apiData.filter((projectData) => {
-					return projectData.projectName.toLowerCase().includes(query.toLowerCase());
-			  });
+	const test = () => {
+		console.log('test'); // ok where is the onClick
+		// im not sure what place it goes, i have two spots. note comments...
+	};
 
 	return (
-		<div className='projects bg-slate-900'>
+		<>
 			{errorMessage !== null && <RateLimit />}
 			{/* Search Component */}
 			<Combobox
@@ -79,41 +91,41 @@ const Projects = () => {
 				onChange={setSelectedProject}
 				className='md:container md:max-w-screen-xl md:mx-auto'
 			>
-				<Combobox.Label className='block text-sm font-medium text-gray-300'>Search by Project Name:</Combobox.Label>
+				<Combobox.Label className='block text-sm font-medium text-gray-700'>Search by Project Name:</Combobox.Label>
 				<div className='relative mt-1'>
 					<Combobox.Input
-						className='w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm'
-						onChange={(event) => setQuery(event.target.value)}
+						className='w-full rounded-md border border-gray-400 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-blue-600 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm'
+						onChange={searchText.bind(this)}
 						placeholder='Search'
-						displayValue={(projectData) => projectData && projectData.projectName}
+						// onclick?
+						displayValue={(projects) => projects && projects.name}
 					/>
 
 					<Combobox.Button className='absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none'>
 						<SelectorIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
 					</Combobox.Button>
-
 					{filterApiData.length > 0 && (
 						<Combobox.Options className='absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
-							{filterApiData.map((projectData) => (
+							{filterApiData.map((projects) => (
 								<Combobox.Option
-									key={projectData.id}
-									value={projectData}
+									key={projects.id}
+									// onclick?
+									value={projects}
 									className={({ active }) =>
 										classNames(
 											'relative cursor-default select-none py-2 pl-3 pr-9',
-											active ? 'bg-indigo-600 text-black' : 'text-gray-900',
+											active ? 'bg-blue-600 text-black' : 'text-gray-900',
 										)
 									}
 								>
 									{({ active, selected }) => (
 										<>
-											<span className={classNames('block truncate', selected && 'font-semibold')}>{projectData.projectName}</span>
-
+											<span className={classNames('block truncate', selected && 'font-semibold')}>{projects.name}</span>
 											{selected && (
 												<span
 													className={classNames(
 														'absolute inset-y-0 right-0 flex items-center pr-4',
-														active ? 'text-black' : 'text-indigo-600',
+														active ? 'text-black' : 'text-blue-600',
 													)}
 												>
 													<CheckIcon className='h-5 w-5' aria-hidden='true' />
@@ -128,34 +140,34 @@ const Projects = () => {
 				</div>
 			</Combobox>
 			{/* Search Component End */}
-			<div className='relative bg-slate-900 pt-5 pb-20 px-4 sm:px-6 lg:pt-8 lg:pb-28 lg:px-8'>
+			<div className='relative bg-gray-100 pt-5 pb-20 px-4 sm:px-6 lg:pt-8 lg:pb-28 lg:px-8'>
 				<div className='absolute inset-0'>
 					<div className='h-1/3 sm:h-2/3' />
 				</div>
 				<div className='relative max-w-7xl mx-auto'>
 					<div className='mt-10 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none'>
 						{/* Card Section Start */}
-						{projects.map((project) => (
-							<div key={project.id} className='flex flex-col rounded-lg shadow-lg overflow-hidden'>
+						{projectSearch.map((item, index) => (
+							<div key={item.id} data='' className='flex flex-col rounded-lg shadow-lg overflow-hidden'>
 								<div className='flex-1 bg-gray-50 p-6 flex flex-col justify-between'>
 									<div className='flex-1'>
-										<p className='text-sm font-medium text-blue-700'>{project.language}</p>
-										<a href={project.html_url} target='_blank' rel='noreferrer noopener' className='hover:underline'>
-											<p className='text-xl font-semibold text-gray-900'>{project.name}</p>
+										<p className='text-sm font-medium text-blue-600'>{item.language}</p>
+										<a href={item.html_url} target='_blank' rel='noreferrer noopener' className='hover:underline'>
+											<p className='text-xl font-semibold text-gray-900'>{item.name}</p>
 										</a>
-										<p className='mt-3 text-base text-gray-600'>{project.description}</p>
+										<p className='mt-3 text-base text-gray-600'>{item.description}</p>
 									</div>
 									<div className='mt-6 flex items-center'>
 										<div className='ml-3'>
-											<p className='text-sm font-medium text-gray-900'>
-												<a href={project.language} className='hover:underline'>
-													{project.full_name}
+											<p className='text-sm font-medium text-gray-800'>
+												<a href={item.language} className='hover:underline'>
+													{item.full_name}
 												</a>
 											</p>
 											<div className='flex space-x-1 text-sm text-gray-500'>
-												<p>{project.created_at}</p>
+												<p>{item.created_at}</p>
 												<span aria-hidden='true'> </span>
-												<a href={project.html_url} target='_blank' rel='noreferrer noopener' className='text-black'>
+												<a href={item.html_url} target='_blank' rel='noreferrer noopener' className='text-black'>
 													View Code
 												</a>
 											</div>
@@ -167,7 +179,7 @@ const Projects = () => {
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
