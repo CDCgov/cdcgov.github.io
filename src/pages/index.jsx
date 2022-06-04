31e0385a9959154b2e260bb4031f7e1cec90ffbf
Jordon <jordontgarcia@gmail.com>
2022-06-03 21:50:21 -0700

    UI Updates/Mobile Responsiveness Improvements

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,12 @@
 // TODO: Jordon: Filterable by Alphabetical, Watchers, size, forks, etc.
-// SEE: https://nextjs.org/docs/basic-features/pages
 // Suggestion, Jordon: Images for projects are a bit redundant? Consumes data, slows client?
 import TitleHeader from '../components/TitleHeader';
-import RateLimit from '../components/rateLimit';
 import { useState } from 'react';
 import { SelectorIcon } from '@heroicons/react/solid';
 import { Combobox } from '@headlessui/react';
 
 console.clear();
 const Home = ({ projects }) => {
-	// const [projects, setProjects] = useState([]);
-	const [errorMessage, setErrorMessage] = useState(null);
-
 	const [query, setQuery] = useState('');
 	const [selectedProject, setSelectedProject] = useState();
 
@@ -36,8 +31,8 @@ const Home = ({ projects }) => {
 		setQuery(projects.name);
 	};
 
-	const formatDate = (x) => {
-		return x.substring(0, 10);
+	const formatDate = (str) => {
+		return str.substring(0, 10);
 	};
 
 	function classNames(...classes) {
@@ -47,7 +42,6 @@ const Home = ({ projects }) => {
 	return (
 		<>
 			<TitleHeader />
-			{Error && <p>{errorMessage}</p>}
 			{/* Search Component Start */}
 			<Combobox
 				as='div'
@@ -55,23 +49,26 @@ const Home = ({ projects }) => {
 				onChange={setSelectedProject}
 				className='md:container md:max-w-screen-xl md:mx-auto'
 			>
-				<Combobox.Label className='block text-sm font-medium text-gray-700'>Search by Project Name:</Combobox.Label>
-				<p className='text-sm font-medium text-gray-700'>
-					Total Projects: <span>{projects.length}</span>
-				</p>
-				<div className='relative mt-1'>
+				<div className='relative w-3/4 laptop:w-full container sm:max-w-screen-xl mx-auto'>
+					<p className='block text-sm font-medium text-gray-800'>
+						Search by Project Name:
+						<p className='text-sm mb-1 font-medium text-gray-800'>
+							Total Projects: <span>{projects.length}</span>
+						</p>
+					</p>
 					<Combobox.Input
-						className='w-full rounded-md border border-gray-400 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-blue-600 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm'
+						className='w-full rounded-sm border border-gray-400 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-blue-600 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm'
 						onChange={searchText.bind(this)}
 						placeholder='Search'
 						displayValue={(projects) => projects && projects.name}
 					/>
 
-					<Combobox.Button className='absolute inset-y-0 right-0 flex projectCards-center rounded-r-md px-2 focus:outline-none'>
-						<SelectorIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
+					<Combobox.Button className='absolute inset-y-0 right-0 flex rounded-r-sm px-2 focus:outline-none'>
+						<SelectorIcon className='h-5 w-5 text-gray-500' aria-hidden='true' />
 					</Combobox.Button>
+
 					{filterApiData.length > 0 && (
-						<Combobox.Options className='absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
+						<Combobox.Options className='absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-sm bg-white py-1 text-base shadow-md ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
 							{filterApiData.map((projects) => (
 								<Combobox.Option
 									key={projects.id}
@@ -80,7 +77,7 @@ const Home = ({ projects }) => {
 									className={({ active }) =>
 										classNames(
 											'relative cursor-default select-none py-2 pl-3 pr-9',
-											active ? 'bg-blue-600 text-black' : 'text-gray-900',
+											active ? 'bg-CDCBlue text-black' : 'text-gray-900',
 										)
 									}
 								>
@@ -91,7 +88,7 @@ const Home = ({ projects }) => {
 												<span
 													className={classNames(
 														'absolute inset-y-0 right-0 flex projectCards-center pr-4',
-														active ? 'text-black' : 'text-blue-700',
+														active ? 'text-black' : 'text-CDCBlue',
 													)}
 												></span>
 											)}
@@ -104,24 +101,24 @@ const Home = ({ projects }) => {
 				</div>
 			</Combobox>
 			{/* Search Component End */}
-			<div className='relative bg-gray-100 pt-5 pb-20 px-4 sm:px-6 lg:pt-8 lg:pb-28 lg:px-8'>
+			<div className='relative bg-gray-50 pt-5 pb-20 px-4 sm:px-6 lg:pt-8 lg:pb-28 lg:px-8'>
 				<div className='absolute inset-0'>
 					<div className='h-1/3 sm:h-2/3' />
 				</div>
 				<div className='relative max-w-7xl mx-auto'>
-					<div className='mt-10 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none'>
+					<div className='mt-4 tablet:mt-7 laptop:mt-10 max-w-xl mx-auto grid gap-6 lg:grid-cols-3 lg:max-w-none'>
 						{/* Card Section Start */}
 						{projectSearch.map((projectCard) => (
-							<div key={projectCard.id} data='' className='flex flex-col rounded-lg shadow-lg overflow-hidden'>
-								<div className='flex-1 bg-gray-50 p-6 flex flex-col justify-between'>
+							<div key={projectCard.id} className='flex flex-col rounded-md shadow-md shadow-gray-300 overflow-hidden'>
+								<div className='flex-1 bg-white p-6 flex flex-col justify-between'>
 									<div className='flex-1'>
-										<p className='text-sm font-medium text-blue-600'>{projectCard.language}</p>
+										<p className='text-sm font-medium text-CDCBlue'>{projectCard.language}</p>
 										<a href={projectCard.html_url} target='_blank' rel='noreferrer noopener' className='hover:underline'>
 											<p className='text-xl font-semibold text-gray-900'>{projectCard.name}</p>
 										</a>
-										<p className='mt-3 text-base text-gray-600'>{projectCard.description}</p>
+										<p className='mt-3 text-sm tablet:text-base text-gray-700'>{projectCard.description}</p>
 									</div>
-									<div className='mt-6 flex projectCards-center'>
+									<div className='mt-3 tablet:mt-6 flex projectCards-center'>
 										<div className='ml-2'>
 											<div className='flex space-x-3'>
 												<p className='text-sm font-medium text-gray-800'>{projectCard.forks} Forks</p>
@@ -133,7 +130,7 @@ const Home = ({ projects }) => {
 												<a href={projectCard.html_url} target='_blank' rel='noreferrer noopener' className='text-black'>
 													<button
 														type='button'
-														className='inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+														className='inline-flex items-center px-2 py-1 tablet:px-2.5 tablet:py-1.5 border border-gray-400 shadow-sm text-xs font-medium rounded-sm text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
 													>
 														View Code
 													</button>
@@ -163,7 +160,7 @@ export async function getStaticProps() {
 		props: {
 			projects: projects.flat(),
 		},
-		revalidate: 300,
+		revalidate: 350,
 	};
 }
 
